691e6a9aaaa51b2a8cd009088ddcce1d52e2f7e8
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-01 20:04:27 +0300

    Add files via upload

---
+++ UsersInput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#!Getting user input
+
+name = input("Enter Your Name: ")
+print("Hello " + name + " !")
+
+#!Entering more information
+
+name1 = input("Enter Your Name: ")
+Age = input("Enter Your Age: ")
+print("Hello " + name1 + " ! You age is " + Age)
+
+
+#!Go ahead and test!!!
